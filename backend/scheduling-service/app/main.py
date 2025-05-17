52aed1c1cee90544126c0aafe5636460259a0092
Xeth-0 <tediberhanu777@gmail.com>
2025-05-17 12:04:56 +0300

    feat: sentry integration

--- backend/scheduling-service/app/main.py
+++ backend/scheduling-service/app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from app.api.endpoints import healthcheck
 from app.api.endpoints import scheduling
+import sentry_sdk
+
+# ! Move the sentry dsn to env.
+sentry_sdk.init(
+    dsn="https://ec5ad676078d626f9095cd8395d34341@o4507213915422720.ingest.de.sentry.io/4509336235737168",
+    send_default_pii=True,
+    enable_tracing=True,
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+    profile_lifecycle="trace",
+)
 
 app = FastAPI(title="Scheduling Service", description="A service for scheduling events")
 
